6e7417851838e7605eb5f5848baa604545a5b88f
russ_smith <russ_smith@685f7672-210f-0410-90b4-fe3ad19314fe>
2005-12-07 02:42:29 +0000

    stricter test for pentium hardware, from Dmitry Marakasov
    git-svn-id: https://opende.svn.sourceforge.net/svnroot/opende/trunk/ode@825 685f7672-210f-0410-90b4-fe3ad19314fe

--- configurator.c
+++ configurator.c
@@ -222,7 +222,7 @@ void check_if_this_is_a_pentium (FILE *file)
   write_header_comment (file,"is this a pentium on a gcc-based platform?");
   write_to_file ("ctest.cpp",
 		 "int main() {\n"
-		 "  asm (\"mov $0,%%eax\\n cpuid\\n\" : : : \"%eax\");\n"
+		 "  asm (\"mov $0,%%eax\\n mov %%eax,(%%esi)\\n cpuid\\n\" : : : \"%eax\");\n"
 		 "  return 0;\n"
 		 "}\n");
   delete_file ("ctest.exe");
